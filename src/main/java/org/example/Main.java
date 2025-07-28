fcf0c8e5e22ee2c951e14de1b9a6a193b71a3eef
Zack <zacharycollier36@protonmail.com>
2025-07-28 14:05:54 -0230

    Added notes for QAP

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,38 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/* Functional API interacting with AWS Database
+* Post screenshots of Postman doing API tests
+* Add new members and tournaments
+* Add members to tournaments
+* Search members by name, membership type, phone number, tourney start date
+* Search tournaments for start date, location
+* Find all members in tournament
+* Add Docker support */
+
+/*
+For Members:
+        ◦ ID (auto generated)
+        ◦ Member Name
+        ◦ Member Address
+        ◦ Member Email Address
+        ◦ Member Phone Number
+        ◦ Start Date of membership
+        ◦ Duration of membership
+For Tournaments:
+        ◦ ID (auto generated)
+        ◦ Start date.
+        ◦ End date.
+        ◦ Location
+        ◦ Entry Fee
+        ◦ Cash Prize Amount
+        ◦ Participating Members
+*/
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
     }
