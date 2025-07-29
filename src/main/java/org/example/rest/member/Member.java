fbafa3a5797613484aadd9d6c0f0fd1e895ad084
Zack <zacharycollier36@protonmail.com>
2025-07-28 21:46:05 -0230

    Added attributes to Member.java and Tournament.java
    - id, entryFee, cashPrize, startDate, and endDate for Tournaments
    - id, name, address, email, phone, membershipStartDate, durationMonths for Members

--- src/main/java/org/example/rest/member/Member.java
+++ src/main/java/org/example/rest/member/Member.java
@@ -1,4 +1,21 @@
 package org.example.rest.member;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Calendar;
+
+
+@Entity
 public class Member {
+    //attributes
+    @Id
+    private long id;
+    private short durationMonths;
+    private Calendar membershipStartDate;
+    private String name;
+    private String address;
+    private String email;
+    private String phone;
+
 }
